908e0da87aca26788a18619741be21f330acdd2b
bgrhnzcn <bugrahanozcan16@gmail.com>
2023-11-09 00:47:38 +0300

    .

--- utils_display.c
+++ utils_display.c
@@ -6,7 +6,7 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:42:40 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2023/11/08 23:22:50 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2023/11/09 00:47:17 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	draw_tri(t_data *data, int j, unsigned int color)
 {
 	draw_line(&(data)->img, data->tris[j].pts[0],
-	data->tris[j].pts[1], color);
+		data->tris[j].pts[1], color);
 	draw_line(&(data)->img, data->tris[j].pts[1],
-	data->tris[j].pts[2], color);
+		data->tris[j].pts[2], color);
 	draw_line(&(data)->img, data->tris[j].pts[2],
-	data->tris[j].pts[0], color);
+		data->tris[j].pts[0], color);
 }
 
 void	fill_img(t_data *data, unsigned int color)
@@ -55,22 +55,24 @@ int	draw_image(t_data *data)
 		tris[i].p1 = mtx3_rot(30, 30, 0, tris[i].p1);
 		tris[i].p2 = mtx3_rot(30, 30, 0, tris[i].p2);
 		tris[i].p3 = mtx3_rot(30, 30, 0, tris[i].p3);
-
 		tris[i].p1 = vec4_to_vec3(mtx_vec_mul4(data->orto_mtx,
-		vec3_to_vec4(tris[i].p1, 1)));
+					vec3_to_vec4(tris[i].p1, 1)));
 		tris[i].p2 = vec4_to_vec3(mtx_vec_mul4(data->orto_mtx,
-		vec3_to_vec4(tris[i].p2, 1)));
+					vec3_to_vec4(tris[i].p2, 1)));
 		tris[i].p3 = vec4_to_vec3(mtx_vec_mul4(data->orto_mtx,
-		vec3_to_vec4(tris[i].p3, 1)));
-
-		tris[i].p1.x += 1.; tris[i].p1.y += 1.;
-		tris[i].p2.x += 1.; tris[i].p2.y += 1.;
-		tris[i].p3.x += 1.; tris[i].p3.y += 1.;
-
-		tris[i].p1.x *= 0.5 * WIDTH; tris[i].p1.y *= 0.5 * HEIGHT;
-		tris[i].p2.x *= 0.5 * WIDTH; tris[i].p2.y *= 0.5 * HEIGHT;
-		tris[i].p3.x *= 0.5 * WIDTH; tris[i].p3.y *= 0.5 * HEIGHT;
-		
+					vec3_to_vec4(tris[i].p3, 1)));
+		tris[i].p1.x += 1.;
+		tris[i].p1.y += 1.;
+		tris[i].p2.x += 1.;
+		tris[i].p2.y += 1.;
+		tris[i].p3.x += 1.;
+		tris[i].p3.y += 1.;
+		tris[i].p1.x *= 0.5 * WIDTH;
+		tris[i].p1.y *= 0.5 * HEIGHT;
+		tris[i].p2.x *= 0.5 * WIDTH;
+		tris[i].p2.y *= 0.5 * HEIGHT;
+		tris[i].p3.x *= 0.5 * WIDTH;
+		tris[i].p3.y *= 0.5 * HEIGHT;
 		draw_tri(data, i, color);
 		i++;
 	}
