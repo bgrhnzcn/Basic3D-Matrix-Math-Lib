260e17107dedd4d6182914854aef62c8990f5a91
bgrhnzcn <bugrahanozcan16@gmail.com>
2023-12-04 20:32:19 +0300

    Debugging

--- colors.c
+++ colors.c
@@ -6,7 +6,7 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 17:34:21 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2023/12/04 02:48:18 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2023/12/04 20:17:39 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,31 @@ t_color	set_color(__uint8_t a, __uint8_t r, __uint8_t g, __uint8_t b)
 t_color	get_gradient_val(t_color from, t_color to, double value)
 {
 	t_color	color;
+	double	temp_red;
+	double	temp_green;
+	double	temp_blue;
 
-	color.red = lerp(value, from.red, to.red);
-	color.green = lerp(value, from.green, to.green);
-	color.blue = lerp(value, from.blue, to.blue);
+	temp_red = lerp(value, from.red, to.red);
+	if (temp_red >= 255)
+		color.red = 255;
+	else if (temp_red <= 0)
+		color.red = 0;
+	else
+		color.red = temp_red;
+	temp_green = lerp(value, from.green, to.green);
+	if (temp_green >= 255)
+		color.green = 255;
+	else if (temp_green <= 0)
+		color.green = 0;
+	else
+		color.green = temp_green;
+	temp_blue = lerp(value, from.blue, to.blue);
+	if (temp_blue >= 255)
+		color.blue = 255;
+	else if (temp_blue <= 0)
+		color.blue = 0;
+	else
+		color.blue = temp_blue;
 	return (color);
 }
 
